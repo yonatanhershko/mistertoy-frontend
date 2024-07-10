502223196a50bcd2a6d8579830c6e812ed117555
Yonatan Hershko <yonher8@gmail.com>
2024-07-10 10:16:30 +0300

    Add some colors

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom"
 export function AppHeader() {
     return (
         <header className="header-container">
-            <h2>Toys</h2>
+            <h2 className="logo">Mister Toy</h2>
             <nav className="nav-links">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/toy">Toys</NavLink>
